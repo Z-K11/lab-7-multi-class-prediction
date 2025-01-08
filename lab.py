aff8d6792e797e73c8b951e54cde196bd12fee0e
Z-K11 <zulqarnainbazidkhail@gmail.com>
2025-01-08 15:46:17 +0500

    call the plt.tight_layout() to fix overlapping in the figure

--- lab.py
+++ lab.py
@@ -16,10 +16,15 @@ x[:,0] selects all rows from the 0th column so x[:,0].min() finds the minimum va
 the rows in column 0 and x[:,0].max() finds the maximum value from all the rows in 0th Column'''
     y_min,y_max=Y[:,0].min()-1,Y[:,0].max()+1
     # this is the same as above explanation
-    xx,yy=np.meshgrid(np.arange(x_min,x_max,plot_step)),np.arange(y_min,y_max,plot_step)
+    xx,yy=np.meshgrid(np.arange(x_min,x_max,plot_step),np.arange(y_min,y_max,plot_step))
     '''np.arange(min,max,step)takes a minimum value a maximum value and a step_size
     what this function does is create a 1d array and populates it with equally spaced
     values between from min to before max
     np.meshgrid(array1,array2) takes 2 1d arrays and combines the to create a grid the first
     is taken as x-axis i.e for rows and the second for y-axist i.e fro clolumns rows repeat
     array 1 values and column repeates array 2 values '''
+    plt.tight_layout(h_pad=0.5,w_pad=0.5,pad=2.5)
+    '''unction in Matplotlib is used to automatically adjust the layout of a figure to ensure 
+    that subplots and other plot elements (like titles, axis labels, and legends) do not overlap.
+    '''
+    
